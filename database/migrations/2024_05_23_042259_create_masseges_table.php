88fc532427f976b10c2c093b97fe833253730d5b
Valodya <zefirzeref@gmail.com>
2024-05-28 15:44:40 +0700

    создание миграций

---
+++ database/migrations/2024_05_23_042259_create_masseges_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('masseges', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('picipient_id')->constrained('users');
+            $table->string('massege');
+            $table->string('is_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('masseges');
+    }
+};
